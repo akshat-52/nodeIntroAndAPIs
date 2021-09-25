ef5f7f4b186d9e2e8cde1ecdfd0d83ed40146ff3
akshat-52 <52.akkey@gmail.com>
2021-09-25 13:10:49 +0530

    added book api

--- Day22/index.js
+++ Day22/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const db = require("./database/db");
+
 const app = express();
 
 const port ='8000';
@@ -7,8 +9,30 @@ const port ='8000';
 // APIs for a Book management system
 // Book, Author and then publications
 
-// GET
+// GET --> 
+// ## all books
+// ## get the specific book based on ISBN
+// ## get the specific book based on category
+// ## all authors
+// ## authors based on a book ISBN
+// ## all publications
+// ## authors belonging to specific publication
 
+
+/** 
+ *
+ * @route  /books
+ * @description "API to get all books"
+ * @method GET
+ * @params
+ * @return_Type JSON Object
+ * 
+*/
+
+app.get("/books", (req, res) =>{
+    var books = db.books;
+    res.json(books);
+})
 
 
 app.listen(port, () =>{
