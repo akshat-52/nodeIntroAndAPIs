1ba89db70df543ffc726443ce117262bd036c6c9
akshat-52 <52.akkey@gmail.com>
2021-09-26 22:21:54 +0530

    Added Grid JS Project

---
+++ projects/assets/js/main.js
@@ -0,0 +1,37 @@
+
+// document.getElementById("gallery").innerHTML=` <div class="col-12 col-md-6 col-lg-3">
+// <img class="img-fluid" src="./assets/images/bunny.jpg" alt="rabbit" height="250px" width="250px">
+// </div>
+// <div class="col-12 col-md-6 col-lg-3">
+// <img class="img-fluid" src="./assets/images/tiger.jpg" alt="tiger" height="250px" width="250px">
+// </div>
+// <div class="col-12 col-md-6 col-lg-3">
+// <img class="img-fluid" src="./assets/images/racoon.jpg" alt="racoon" height="250px" width="250px">
+// </div>
+// <div class="col-12 col-md-6 col-lg-3">
+// <img class="img-fluid" src="./assets/images/lion.jpg" alt="lion" height="250px" width="250px">
+// </div>
+// <div class="col-12 col-md-6 col-lg-3">
+// <img class="img-fluid" src="./assets/images/cat.jpg" alt="cat" height="250px" width="250px">
+// </div>
+// <div class="col-12 col-md-6 col-lg-3">
+// <img class="img-fluid" src="./assets/images/cow.jpg" alt="cow" height="250px" width="250px">
+// </div>
+// <div class="col-12 col-md-6 col-lg-3">
+// <img class="img-fluid" src="./assets/images/dog.jpg" alt="dog" height="250px" width="250px">
+// </div>
+// <div class="col-12 col-md-6 col-lg-3">
+// <img class="img-fluid" src="./assets/images/deer.jpg" alt="deer" height="250px" width="250px">
+// </div>`;
+
+// function getInnerHTML() {
+//     return gridHTMLStr;
+// }
+
+
+let gridHTMLStr='';
+
+
+function updateGRID(){
+    document.getElementById("gallery").innerHTML=gridHTMLStr
+}
